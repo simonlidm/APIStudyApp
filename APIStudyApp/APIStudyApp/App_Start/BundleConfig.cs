340977b305da7f961737f2bc10ecf1cada12e618
simonlidm <simon.lindmer@gmail.com>
2018-03-26 21:29:57 +0200

    Submeny for Facit new fontstyle

--- APIStudyApp/APIStudyApp/App_Start/BundleConfig.cs
+++ APIStudyApp/APIStudyApp/App_Start/BundleConfig.cs
@@ -21,11 +21,11 @@ namespace APIStudyApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/font-awesome.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/Content/custom.css"
+                      "~/Content/font-awesome.min.css"
+                      
                       ));
         }
     }
